4f23e971e8edf381d0b13ecf4bfc4948e35486c9
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-20 22:26:58 +0100

    learning react again

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom';
 import JSON from './db.json';
 
 // COMPONENTS
-import Header from './components/header';
-import PlaylistHeader from './components/playlist-header';
-import NewsList from './components/news-list';
 
 class App extends Component {
 
