2a2d7b8e4ad9a00c2a1c8d6c3114835f002efece
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-21 23:12:21 +0100

    click event

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const handleClick = () => {
+    console.log("hello ninja")
+}
+
 const Home = () => {
     return (
         <div className="home">
             <h1>Home page</h1>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
