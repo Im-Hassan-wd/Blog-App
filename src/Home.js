edb5150118e061199a7ca4e97acedcef33406ae4
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-21 23:22:48 +0100

    whts happening

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const handleClick = () => {
-    console.log("hello ninja")
-}
-
 const Home = () => {
+    let name = "Jerry";
+
+    const handleClick = () => {
+        name = "Luchi";
+    }
+
     return (
         <div className="home">
             <h1>Home page</h1>
+            <p>{ name }</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
