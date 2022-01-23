a967bf45d88e79df2ea4c32cb294082a0a4e11ac
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-23 23:44:17 +0100

    outputting list

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,18 @@ import React, {useState} from 'react';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        {"id": 1,"title": "New ES6 upgrade available","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto"},
-        {"id": 1,"title": "Welcome party","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto"},
-        {"id": 1,"title": "Wed dev top tips","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto"},
+        {"id": 1,"title": "New ES6 upgrade available","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto", author: "mario"},
+        {"id": 2,"title": "Welcome party","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto", author: "yoshi"},
+        {"id": 3,"title": "Wed dev top tips","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto", author: "mario"},
     ]);
     return (
         <div className="home">
+            {blogs.map((blog) => (
+                <div className="preview" key={blog.id}> 
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
