2b82b47f2c2d95ae06f0f63b3f884807ff1ab72b
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-24 21:20:41 +0100

    delete event

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,14 @@ const Home = () => {
         {"id": 2,"title": "Welcome party","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto", author: "yoshi"},
         {"id": 3,"title": "Wed dev top tips","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto", author: "mario"},
     ]);
+
+    const handleDelete = () => {
+
+    }
+
     return (
         <div className="home">
-           <BlogList blogs={blogs} title="All Blogs !"/> 
-           <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's blogs !"/> 
+           <BlogList blogs={blogs} title="All Blogs !" handleDelete={handleDelete} /> 
         </div>
     );
 }
