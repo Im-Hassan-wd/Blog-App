31d80a273a7ef2b3098c23ff686e41dadf5ff4cb
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-25 21:34:13 +0100

    useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,14 +8,22 @@ const Home = () => {
         {"id": 3,"title": "Wed dev top tips","feed": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto", author: "mario"},
     ]);
 
+    const [name, setName ] = useState("mario");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log("use effect fired");
+    }, []);
+
     return (
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs !" handleDelete={handleDelete} /> 
+           <button onClick={() => setName("luigi")}>change name</button>
+           <p>{ name }</p>
         </div>
     );
 }
