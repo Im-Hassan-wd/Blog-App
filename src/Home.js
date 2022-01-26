44c97cc866d89c58a6abdb4ecabedd549d096d27
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-26 22:26:09 +0100

    fetched the data, handled the err and conditional loading

--- src/Home.js
+++ src/Home.js
@@ -4,28 +4,32 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
-            fetch("http://localhost:8000/blog")
+            fetch("http://localhost:8000/blogs")
                 .then(response => {
                     if(!response.ok) {
                         throw Error("could not fetch the data for that resource");
                     }
-                    // return response.json();
+                    return response.json();
                 })
                 .then(data => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
                 })
                 .catch(err => {
-                    console.log(err.message);
+                    setIsPending(false);
+                    setError(err.message);
                 });
         }, 2000);
     }, []);
 
     return (
         <div className="home">
+            { error && <div>{ error }</div>}
             { isPending && <div>Loading...</div> }
            {blogs && <BlogList blogs={blogs} title="All Blogs !" />}
         </div>
