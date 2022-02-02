ece9555649418d429ea6296959be0001fdca0f0d
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-02 23:39:44 +0100

    styling things up

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import scarescrow from "./Scarecrow.png";
 
 const NotFound = () => {
     return (
         <div className="not-found">
-            <div class="logo">404 NOT FOUND</div>
-            <div class="error">
-                <img class="error-image" src="../public/Scarecrow.png" alt="Scarecrow error image" />
-                <div class="content">
+            <div className="logo">404 NOT FOUND</div>
+            <div className="error">
+                <img className="error-image" src={scarescrow} alt="Scarecrow error" />
+                <div className="content">
                     <h1>I have bad news for you</h1>
                     <p>The page you are looking for might be removed or is temporarily unavailable</p>
-                    <Link to="/"></Link>
+                    <Link to="/">Back to homepage</Link>
                 </div>
             </div>
         </div>
