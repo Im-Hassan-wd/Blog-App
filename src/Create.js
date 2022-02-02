26ed754bcd81d6bbb68dbbb1bc5ddfecc269bc94
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-02 05:33:13 +0100

    create component

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,18 @@
 const Create = () => {
     return (
         <div className="create">
-            <h2>Add new blog!</h2>
+            <h2>Add a New blog!</h2>
+            <form>
+                <label>Blog title:</label>
+                <input type="text" required />
+                <label>Blog body:</label>
+                <textarea required></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+            </form>
         </div>
     );
 }
